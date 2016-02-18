4cecb559d50a8e3ba7f72a54938708fd54b0a9a2
Mouamle <hassan.mouamle@yahoo.com>
2016-02-18 21:42:14 +0300

    Update Arithmeticprogression.lua

--- Arithmeticprogression.lua
+++ Arithmeticprogression.lua
@@ -10,3 +10,12 @@ function solve(a, n, d)
   Un = " { " .. tostring(a + (n-1)*d) .. " } "
   return tempstr .. "\n" .. "The value when n = { " .. n .. " } is:" .. Un;
 end
+
+-- example :
+--[[
+print(solve(7, 6, -3))
+    output{
+        | 7 | | 4 | | 1 | | -2 | | -5 | | -8 | | -11 | | -14 | | -17 |
+        The value when n = { 6 } is: { -8 }
+    }
+]]
